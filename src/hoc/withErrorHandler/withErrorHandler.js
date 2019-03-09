c624192a0ae6ccaaded83398754e4df80afef443
Ekaterina <ek.zhuravleva@gmail.com>
2019-03-09 18:27:36 +0100

    Retrieving data from the backend

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,7 +7,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
         state = {
             error: null
         }
-        componentDidMount() {
+        componentWillMount() {
             axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
@@ -25,7 +25,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
                 <Auxiliary>
                     <Modal 
                         show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}>
+                        modalClosed ={this.errorConfirmedHandler}>
                         {this.state.error ? this.state.error.message: null}
                     </Modal>
                     <WrappedComponent {...this.props} />
